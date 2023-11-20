8a6c464cd57f3c8eec19ea0ee19a487a485c32ae
Ben Wojtowicz <Bwojtowicz4483@stu.d214.org>
2023-11-20 15:11:03 -0600

    all fixed

---
+++ CPB_Firebase/CollegeModel.swift
@@ -0,0 +1,8 @@
+//
+//  CollegeModel.swift
+//  CPB_Firebase
+//
+//  Created by Ben Wojtowicz on 11/20/23.
+//
+
+import Foundation
---
+++ CPB_Firebase/CollegeViewmodel.swift
@@ -0,0 +1,8 @@
+//
+//  CollegeViewmodel.swift
+//  CPB_Firebase
+//
+//  Created by Ben Wojtowicz on 11/20/23.
+//
+
+import Foundation
